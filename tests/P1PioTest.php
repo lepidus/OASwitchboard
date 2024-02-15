7f883bda6ad10d7cf247789280ce7c4b91183f86
yves <yves@lepidus.com.br>
2024-02-15 18:28:55 -0400

    Refactors tests

--- tests/P1PioTest.php
+++ tests/P1PioTest.php
@@ -1,7 +1,42 @@
 <?php
 
-trait P1PioArrayGenerator
+import('lib.pkp.tests.PKPTestCase');
+import('plugins.generic.OASwitchboardForOJS.messages.P1Pio');
+
+class P1PioTest extends PKPTestCase
 {
+    private $p1Pio;
+    private $json;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->p1Pio = new P1Pio();
+        $this->json = json_decode($this->p1Pio->getMessage());
+    }
+
+    public function testP1PioJsonHeader()
+    {
+        $header = $this->json->header;
+        $this->assertEquals('p1', $header->type);
+        $this->assertEquals('v2', $header->version);
+        $this->assertEquals($this->getToSendMessageObject(), $header->to);
+        $this->assertEquals($this->getFromMessageObject(), $header->from);
+        $this->assertEquals('0000-0001', $header->ref);
+        $this->assertEquals("2024-04-01", $header->validity);
+        $this->assertEquals(true, $header->persistent);
+        $this->assertEquals(true, $header->pio);
+    }
+
+    public function testP1PioJsonData()
+    {
+        $data = $this->json->data;
+        $this->assertEquals('VoR', $data->timing);
+        $this->assertEquals($this->getAuthorsArray(), $data->authors);
+        $this->assertEquals($this->getArticleObject(), $data->article);
+        $this->assertEquals($this->getJournalArray(), $data->journal);
+    }
+
     private function getToSendMessageObject()
     {
         return (object)[
