b966da5ded67151d257de368d5f882a7aab7c7cf
yves <yves@lepidus.com.br>
2024-02-15 12:48:54 -0400

    Add P1-PIO JSON Object.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#754
    
    Signed-off-by: yves <yves@lepidus.com.br>

---
+++ JsonP1PioObject.inc.php
@@ -0,0 +1,99 @@
+<?php
+
+class JsonP1PioObject
+{
+    public function getJson()
+    {
+        $data = [
+            "timing" => "VoR",
+            "authors" => [
+                [
+                    "listingorder" => 1,
+                    "listingorderAtAcceptance" => 1,
+                    "listingorderAtSubmission" => 1,
+                    "lastName" => "Baggins",
+                    "firstName" => "Frodo",
+                    "initials" => "FB",
+                    "ORCID" => "0000-0000-0000-0000",
+                    "creditroles" => [ "writing", "visualization" ],
+                    "isCorrespondingAuthor" => true,
+                    "isCorrespondingAuthorAtAcceptance" => true,
+                    "isCorrespondingAuthorAtSubmission" => true,
+                    "collaboration" => "Laboratory collaboration",
+                    "institutions" => [
+                        [
+                            "sourceaffiliation" => "University of Amsterdam, Department Computer Science",
+                            "name" => "University of Amsterdam",
+                            "ror" => "https://ror.org/04dkp9463",
+                            "isni" => "0000 0000 8499 2262",
+                            "country" => "NL"
+                        ]
+                    ],
+                    "currentaddress" => [
+                        [
+                            "name" => "University of Amsterdam",
+                            "ror" => "https://ror.org/04dkp9463",
+                            "isni" => "0000 0000 8499 2262"
+                        ]
+                    ],
+                    "affiliation" => "University of Amsterdam"
+                ]
+            ],
+            "article" => [
+                "title" => "The International relations of Middle-Earth",
+                "doi" => "https://doi.org/00.0000/mearth.0000",
+                "submissionId" => "00.0000",
+                "type" => "research-article",
+                "funders" => [
+                    [
+                        "name" => "Aragorn Foundation",
+                        "ror" => "https://ror.org/999999",
+                        "fundref" => "501100000000"
+                    ],
+                    [
+                        "name" => "Middle-Earth Thinktank",
+                        "ror" => "https://ror.org/888888",
+                        "fundref" => "501100000001"
+                    ]
+                ],
+                "acknowledgement" => "Aragorn Foundation, Middle-Earth Thinktank",
+                "grants" => [
+                    [
+                        "name" => "Generous grant",
+                        "id" => "GD-000-001",
+                        "doi" => "https://doi.org/00.0000/00-2020-000000"
+                    ]
+                ],
+                "manuscript" => [
+                    "dates" => [
+                        "submission" => "2021-02-01",
+                        "acceptance" => "2021-03-01",
+                        "publication" => "2021-04-01"
+                    ],
+                    "id" => "00.0000-000"
+                ],
+                "preprint" => [
+                    "title" => "The International relations of Middle-Earth",
+                    "url" => "https://arxiv.org/00.0000-000",
+                    "id" => "00.0000-000"
+                ],
+                "vor" => [
+                    "publication" => "pure OA journal",
+                    "license" => "CC BY",
+                    "deposition" => "open repository, like PMC",
+                    "researchdata" => "data available on request",
+                    "startdate" => "2024-01-01"
+                ]
+            ],
+            "journal" => [
+                "name" => "Middle Earth papers",
+                "id" => "0000-0001",
+                "issn" => "0000-0001",
+                "eissn" => "0000-0002",
+                "inDOAJ" => true,
+                "typecomment" => "Diamond"
+            ]
+        ];
+        return json_encode($data);
+    }
+}
