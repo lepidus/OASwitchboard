96281a515e4258229aaaaa039ddf8667290147a7
yves <yves@lepidus.com.br>
2024-02-15 16:26:46 -0400

    Add header data.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#754
    
    Signed-off-by: yves <yves@lepidus.com.br>

--- tests/CreateJsonObjectForP1PioTest.php
+++ tests/CreateJsonObjectForP1PioTest.php
@@ -8,11 +8,35 @@ class CreateJsonObjectForP1PioTest extends PKPTestCase
     public function testCreateJSONObjectForP1PIO()
     {
         $p1Pio = new P1Pio();
-        $data = json_decode($p1Pio->getMessage());
-        $this->assertEquals('VoR', $data->timing);
-        $this->assertEquals($this->getAuthorsArray(), $data->authors);
-        $this->assertEquals($this->getArticleObject(), $data->article);
-        $this->assertEquals($this->getJournalArray(), $data->journal);
+        $json = json_decode($p1Pio->getMessage());
+        $this->assertEquals('p1', $json->header->type);
+        $this->assertEquals('v2', $json->header->version);
+        $this->assertEquals($this->getToSendMessageObject(), $json->header->to);
+        $this->assertEquals($this->getFromMessageObject(), $json->header->from);
+        $this->assertEquals('0000-0001', $json->header->ref);
+        $this->assertEquals("2024-04-01", $json->header->validity);
+        $this->assertEquals(true, $json->header->persistent);
+        $this->assertEquals(true, $json->header->pio);
+        $this->assertEquals('VoR', $json->data->timing);
+        $this->assertEquals($this->getAuthorsArray(), $json->data->authors);
+        $this->assertEquals($this->getArticleObject(), $json->data->article);
+        $this->assertEquals($this->getJournalArray(), $json->data->journal);
+    }
+
+    private function getToSendMessageObject()
+    {
+        return (object)[
+            "address" => "https://ror.org/xxxxxxxx",
+            "name" => "Science Publisher"
+        ];
+    }
+
+    private function getFromMessageObject()
+    {
+        return (object)[
+            "address" => "https://ror.org/04dkp9463",
+            "name" => "University of Amsterdam"
+        ];
     }
 
     private function getAuthorsArray()
