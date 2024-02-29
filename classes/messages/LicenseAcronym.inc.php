f160d54667a6648747a60d51b389e79ce59e9392
yves <yves@lepidus.com.br>
2024-02-29 17:59:34 -0400

    Finish: refactors code.
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#763
    
    Signed-off-by: iudi <iudi@lepidus.com.br>
    Signed-off-by: yves <yves@lepidus.com.br>

---
+++ classes/messages/LicenseAcronym.inc.php
@@ -0,0 +1,30 @@
+<?php
+
+trait LicenseAcronym
+{
+    private function getLicenseAcronym($licenseURL)
+    {
+        $licenseAcronymMap = array(
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-nc-nd/4.0[/]?|' => 'CC BY-NC-ND',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-nc/4.0[/]?|' => 'CC BY-NC',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-nc-sa/4.0[/]?|' => 'CC BY-NC-SA',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-nd/4.0[/]?|' => 'CC BY-ND',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by/4.0[/]?|' => 'CC BY',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-sa/4.0[/]?|' => 'CC BY-SA',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-nc-nd/3.0[/]?|' => 'CC BY-NC-ND',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-nc/3.0[/]?|' => 'CC BY-NC',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-nc-sa/3.0[/]?|' => 'CC BY-NC-SA',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-nd/3.0[/]?|' => 'CC BY-ND',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by/3.0[/]?|' => 'CC BY',
+            '|http[s]?://(www\.)?creativecommons.org/licenses/by-sa/3.0[/]?|' => 'CC BY-SA',
+            '|http[s]?://(www\.)?creativecommons.org/publicdomain/zero/1.0[/]?|' => 'CC0'
+        );
+
+        foreach ($licenseAcronymMap as $pattern => $acronym) {
+            if (preg_match($pattern, $licenseURL ?? '')) {
+                return $acronym;
+            }
+        }
+        return null;
+    }
+}
