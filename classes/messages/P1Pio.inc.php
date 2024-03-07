87a3bbc1e4f2bb4894f2bf460004f0cdbb59dc42
iudizm <iudi@lepidus.com.br>
2024-03-07 19:45:57 -0300

    Refactor submission data validation to simplify
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#770
    
    Signed-off-by: iudi <iudi@lepidus.com.br>
    Signed-off-by: yves <yves@lepidus.com.br>

--- classes/messages/P1Pio.inc.php
+++ classes/messages/P1Pio.inc.php
@@ -1,7 +1,6 @@
 <?php
 
 import('plugins.generic.OASwitchboardForOJS.classes.messages.P1PioDataFormat');
-import('plugins.generic.OASwitchboardForOJS.classes.messages.ValidateSubmisionMandatoryFields');
 import('plugins.generic.OASwitchboardForOJS.classes.messages.LicenseAcronym');
 import('classes.submission.Submission');
 
@@ -9,7 +8,6 @@ class P1Pio
 {
     use P1PioDataFormat;
     use LicenseAcronym;
-    use ValidateSubmisionMandatoryFields;
 
     private $submission;
     private const ARTICLE_TYPE = 'research-article';
@@ -103,5 +101,32 @@ class P1Pio
     public function getContent(): array
     {
         return $this->assembleMessage();
+    }
+
+    public function validateSubmissionHasMandatoryData(): array
+    {
+        $data = $this->getContent()['data'];
+        $missingDataMessages = [];
+
+        foreach ($data['authors'] as $key => $author) {
+            if (!isset($author['lastName'])) {
+                $missingDataMessages[] = "The family name name of an author must be present.";
+            }
+            if (!isset($author['affiliation'])) {
+                $missingDataMessages[] = "Affiliation of an author must be set.";
+            }
+            $institution = $author['institutions'][0];
+            if (!isset($institution['name'])) {
+                $missingDataMessages[] = "Affiliation of an author must be set.";
+            }
+        }
+        if (!isset($data['article']['doi'])) {
+            $missingDataMessages[] = "The article must have a DOI associated.";
+        }
+        if (!isset($data['journal']['id'])) {
+            $missingDataMessages[] = "The journal must have a ISSN or eISSN assigned.";
+        }
+
+        return $missingDataMessages;
     }
 }
