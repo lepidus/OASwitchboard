bc5c74b62cd8855144d65549663d92393af7428b
yves <yves@lepidus.com.br>
2024-08-01 17:29:44 -0400

    Removes reload page from Cypress tests.
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#851
    
    Signed-off-by: Yves <yves@lepidus.com.br>
    Signed-off-by: Iudi <iudi@lepidus.com.br>

--- cypress/tests/Test4_sendMessageSuccess.cy.js
+++ cypress/tests/Test4_sendMessageSuccess.cy.js
@@ -38,7 +38,6 @@ describe('Send P1-PIO message with success', function () {
 
         cy.get('.pkpPublication__header > .pkpHeader__actions > button.pkpButton').contains("Schedule For Publication").click();
         cy.get('.pkpFormPage__footer button:contains("Publish")').click();
-        cy.reload();
 
         cy.get('.app__notifications').contains("At least one author of the article must have a ROR associated with their affiliation.");
         cy.get('.app__notifications').contains("The message was successfully sent to the OA Switchboard");
