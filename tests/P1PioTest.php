9d7b5e1037bbbd4f883d1884ebd7a763a2ab9091
iudi <iudi@lepidus.com.br>
2026-09-15 14:37:28 -0300

    Include the Funding plugin award numbers as grants in the P1 message
    Signed-off-by: iudi <iudi@lepidus.com.br>

--- tests/P1PioTest.php
+++ tests/P1PioTest.php
@@ -4,6 +4,7 @@ namespace APP\plugins\generic\OASwitchboard\tests;
 
 use APP\plugins\generic\OASwitchboard\tests\helpers\ObjectFactory;
 use APP\plugins\generic\OASwitchboard\tests\helpers\P1PioExpectedTestData;
+use PKP\core\Registry;
 use PKP\tests\PKPTestCase;
 
 class P1PioTest extends PKPTestCase
@@ -19,6 +20,12 @@ class P1PioTest extends PKPTestCase
         $this->mockRequest();
         $journal = ObjectFactory::createMockedJournal($onlineIssn = '0000-0001', $printIssn = '0000-0002');
         $this->submission = ObjectFactory::createTestSubmission($journal);
+        ObjectFactory::registerStubFundingPlugin([[
+            'id' => 1,
+            'name' => 'Universidade Federal de Santa Catarina',
+            'identification' => 'http://dx.doi.org/10.13039/501100007082',
+            'awardNumbers' => [10 => '2021/12345-6'],
+        ]]);
         $this->P1Pio = ObjectFactory::createP1PioMock($this->submission);
     }
 
@@ -29,7 +36,7 @@ class P1PioTest extends PKPTestCase
 
     protected function getMockedRegistryKeys(): array
     {
-        return [...parent::getMockedRegistryKeys(), 'site'];
+        return [...parent::getMockedRegistryKeys(), 'site', 'plugins', 'daos'];
     }
 
     public function testGetAuthorGivenName()
@@ -108,6 +115,71 @@ class P1PioTest extends PKPTestCase
         $vor = $articleData['vor'];
         $this->assertEquals('pure OA journal', $vor['publication']);
         $this->assertEquals('CC BY-NC-ND', $vor['license']);
+    }
+
+    public function testArticleGrantsShouldListAwardNumbersOfAllFunders()
+    {
+        ObjectFactory::registerStubFundingPlugin([
+            [
+                'id' => 1,
+                'name' => 'Fundação de Amparo à Pesquisa do Estado de São Paulo',
+                'identification' => 'https://doi.org/10.13039/501100001807',
+                'awardNumbers' => [10 => '2021/12345-6', 11 => '2022/65432-1'],
+            ],
+            [
+                'id' => 2,
+                'name' => 'Conselho Nacional de Desenvolvimento Científico e Tecnológico',
+                'identification' => 'https://doi.org/10.13039/501100003593',
+                'awardNumbers' => [12 => 'CNPq-999'],
+            ],
+        ]);
+
+        $articleData = $this->P1Pio->getArticleData();
+
+        $this->assertEquals(
+            [['id' => '2021/12345-6'], ['id' => '2022/65432-1'], ['id' => 'CNPq-999']],
+            $articleData['grants']
+        );
+    }
+
+    public function testArticleShouldOmitGrantsWhenFundersHaveNoAwardNumbers()
+    {
+        ObjectFactory::registerStubFundingPlugin([[
+            'id' => 1,
+            'name' => 'Universidade Federal de Santa Catarina',
+            'identification' => 'http://dx.doi.org/10.13039/501100007082',
+            'awardNumbers' => [],
+        ]]);
+
+        $articleData = $this->P1Pio->getArticleData();
+
+        $this->assertArrayHasKey('funders', $articleData);
+        $this->assertArrayNotHasKey('grants', $articleData);
+    }
+
+    public function testArticleShouldOmitFundersAndGrantsWhenFundingPluginIsDisabled()
+    {
+        ObjectFactory::registerStubFundingPlugin([[
+            'id' => 1,
+            'name' => 'Universidade Federal de Santa Catarina',
+            'identification' => 'http://dx.doi.org/10.13039/501100007082',
+            'awardNumbers' => [10 => '2021/12345-6'],
+        ]], $enabled = false);
+
+        $articleData = $this->P1Pio->getArticleData();
+
+        $this->assertArrayNotHasKey('funders', $articleData);
+        $this->assertArrayNotHasKey('grants', $articleData);
+    }
+
+    public function testArticleShouldOmitFundersAndGrantsWhenFundingPluginIsNotInstalled()
+    {
+        Registry::delete('plugins');
+
+        $articleData = $this->P1Pio->getArticleData();
+
+        $this->assertArrayNotHasKey('funders', $articleData);
+        $this->assertArrayNotHasKey('grants', $articleData);
     }
 
     public function testGetJournalName()
--- tests/helpers/ObjectFactory.php
+++ tests/helpers/ObjectFactory.php
@@ -13,6 +13,8 @@ use PKP\db\DAORegistry;
 use PKP\decision\Decision;
 use PKP\doi\Doi;
 use PKP\galley\Galley;
+use PKP\plugins\Plugin;
+use PKP\plugins\PluginRegistry;
 use PKP\site\Site;
 use PKP\submissionFile\SubmissionFile;
 
@@ -169,13 +171,100 @@ class ObjectFactory
             {
                 return $this->stubDecisions;
             }
-            public function getFundersData(): array
+        };
+    }
+
+    /**
+     * Registers a stand-in for the Funding plugin and its DAOs.
+     *
+     * @param array $funders Each entry has 'id', 'name', 'identification' and 'awardNumbers'
+     */
+    public static function registerStubFundingPlugin(array $funders, bool $enabled = true): void
+    {
+        $fundingPlugin = new class ($enabled) extends Plugin {
+            private bool $enabled;
+            public function __construct(bool $enabled)
             {
-                return [[
-                    'name' => 'Universidade Federal de Santa Catarina',
-                    'fundref' => 'http://dx.doi.org/10.13039/501100007082',
-                ]];
+                parent::__construct();
+                $this->enabled = $enabled;
+            }
+            public function getName()
+            {
+                return 'FundingPlugin';
+            }
+            public function getDisplayName()
+            {
+                return 'Funding';
+            }
+            public function getDescription()
+            {
+                return 'Funding';
+            }
+            public function getEnabled()
+            {
+                return $this->enabled;
             }
         };
+        $plugins = & PluginRegistry::getPlugins();
+        $plugins['generic']['FundingPlugin'] = $fundingPlugin;
+
+        DAORegistry::registerDAO('FunderDAO', new class ($funders) {
+            private array $funders;
+            public function __construct(array $funders)
+            {
+                $this->funders = $funders;
+            }
+            public function getBySubmissionId($submissionId)
+            {
+                $funders = array_map(fn ($funder) => new class ($funder) {
+                    private array $funder;
+                    public function __construct(array $funder)
+                    {
+                        $this->funder = $funder;
+                    }
+                    public function getId()
+                    {
+                        return $this->funder['id'];
+                    }
+                    public function getFunderName()
+                    {
+                        return $this->funder['name'];
+                    }
+                    public function getFunderIdentification()
+                    {
+                        return $this->funder['identification'];
+                    }
+                }, $this->funders);
+
+                return new class ($funders) {
+                    private array $funders;
+                    public function __construct(array $funders)
+                    {
+                        $this->funders = $funders;
+                    }
+                    public function next()
+                    {
+                        return array_shift($this->funders);
+                    }
+                };
+            }
+        });
+
+        DAORegistry::registerDAO('FunderAwardDAO', new class ($funders) {
+            private array $funders;
+            public function __construct(array $funders)
+            {
+                $this->funders = $funders;
+            }
+            public function getFunderAwardNumbersByFunderId($funderId)
+            {
+                foreach ($this->funders as $funder) {
+                    if ($funder['id'] === $funderId) {
+                        return $funder['awardNumbers'];
+                    }
+                }
+                return [];
+            }
+        });
     }
 }
